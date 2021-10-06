5e8cc633da9cd180e235ea8336468464e10ceef8
kjhabc2002 <juhju2022@gmail.com>
2021-10-06 16:26:37 +0900

    Add : posting앱 model구현완료

--- postings/models.py
+++ postings/models.py
@@ -1,3 +1,23 @@
-from django.db import models
+from django.db       import models
+from users.models    import User
+from products.models import Product
+from users.models    import TimeStampedModel
 
-# Create your models here.
+class Posting(TimeStampedModel):
+    user_id         = models.ForeignKey('users.User', on_delete=models.CASCADE)
+    posting_id      = models.ForeignKey('postings.Product', on_delete=models.CASCADE)
+    title           = models.CharField(max_length=100)
+    content         = models.TextField()
+    rating          = models.IntegerField()
+
+    class Meta:
+        db_table = 'postings'
+        
+class Comment(TimeStampedModel):
+    user_id         = models.ForeignKey('users.User', on_delete=models.CASCADE)
+    product_id      = models.ForeignKey('postings.Product', on_delete=models.CASCADE)
+    postings_id     = models.ForeignKey('Posting', on_delete=models.CASCADE)
+    contents        = models.TextField()
+  
+    class Meta:
+        db_table = 'comments'  
