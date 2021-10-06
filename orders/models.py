13c4fbb9cfbf35b25fcecea7f85fc15b10b9ddb1
kylee <leeky940926@gmail.com>
2021-10-06 16:18:50 +0900

    products, orders

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,21 @@
-from django.db import models
+from django.db    import models
 
-# Create your models here.
+from users.models import TimeStampedModel
+
+class Wishlist(TimeStampedModel) :
+
+    user    = models.ForeignKey('users.User', on_delete=models.CASCADE)
+    product = models.ForeignKey('products.Product', on_delete=models.CASCADE)
+
+    class Meta :
+
+        db_table = 'wishlists'
+
+class Basket(TimeStampedModel) :
+
+    user    = models.ForeignKey('users.User', on_delete=models.CASCADE)
+    product = models.ForeignKey('products.Product', on_delete=models.CASCADE)
+
+    class Meta :
+
+        db_table = 'baskets'
